e23ac2df63168d222adce3ad63c03b1065676d77
= <tylers@rappid.design>
2021-02-15 17:34:31 -0500

    Level decoration

--- Assets/Game/Scripts/CameraController.cs
+++ Assets/Game/Scripts/CameraController.cs
@@ -10,6 +10,9 @@ public class CameraController : MonoBehaviour
     public Transform targetTransform;
     public Vector3 targetOffset = new Vector3(0f, 0f, -10f);
 
+    public Camera targetCamera;
+    public float targetProjectionSize = 4f;
+
     public float xTime = 0.6f;
     public float xMaxSpeed = 10f;
 
@@ -19,12 +22,17 @@ public class CameraController : MonoBehaviour
     public float zTime = 0.6f;
     public float zMaxSpeed = 12f;
 
+    public float pTime = 0.6f;
+    public float pMaxSpeed = 24f;
+
     [SerializeField] private float _xVelocity = 0f;
     [SerializeField] private float _yVelocity = 0f;
     [SerializeField] private float _zVelocity = 0f;
+    [SerializeField] private float _pVelocity = 0f;
 
     // Update is called once per frame
     private void Start() {
+        if (targetCamera == null) targetCamera = Camera.main;
         TeleportToTarget();
     }
 
@@ -35,6 +43,8 @@ public class CameraController : MonoBehaviour
         float x = Mathf.SmoothDamp(transform.position.x, targetPosition.x, ref _xVelocity, xTime, xMaxSpeed, Time.fixedDeltaTime);
         float y = Mathf.SmoothDamp(transform.position.y, targetPosition.y, ref _yVelocity, yTime, yMaxSpeed, Time.fixedDeltaTime);
         float z = Mathf.SmoothDamp(transform.position.z, targetPosition.z, ref _zVelocity, zTime, zMaxSpeed, Time.fixedDeltaTime);
+
+        float size = Mathf.SmoothDamp(targetCamera.orthographicSize, targetProjectionSize, ref _pVelocity, pTime, pMaxSpeed, Time.fixedDeltaTime);
 
         Vector3 newPosition = new Vector3(x, y, z);
         if (useBounds) {
@@ -53,12 +63,14 @@ public class CameraController : MonoBehaviour
     public void TeleportToTarget() {
         Vector3 targetPosition = GetTarget() + targetOffset;
         transform.position = targetPosition;
+        targetCamera.orthographicSize = targetProjectionSize;
         ResetVelocity();
     }
 
-    public void TeleportTo(Vector3 point) {
+    public void TeleportTo(Vector3 point, float pSize) {
         Vector3 targetPosition = point + targetOffset;
         transform.position = targetPosition;
+        targetCamera.orthographicSize = targetProjectionSize;
         ResetVelocity();
     }
 
@@ -66,6 +78,7 @@ public class CameraController : MonoBehaviour
         _xVelocity = 0f;
         _yVelocity = 0f;
         _zVelocity = 0f;
+        _pVelocity = 0f;
     }
 
     public void OnDrawGizmosSelected() {
---
+++ Assets/ScrollingBackground.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<Transform> objects;
+    public Camera mainCamera;
+
+    public Vector2 scrollSpeed = new Vector2(1f, 0f);
+
+    public bool autoXDistance = true;
+    public float xDistance = 15f;
+
+    void Start()
+    {
+        if (objects.Count < 3) throw new UnityException("ScrollingBackground requires at least 3 objects");
+        if (autoXDistance) {
+            xDistance = Mathf.Abs(objects[0].position.x - objects[1].position.x);
+        }
+        if (!mainCamera) mainCamera = Camera.main;
+
+        objects.Sort((t1, t2) => t1.position.x >= t2.position.x ? 1 : -1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Transform t in objects) {
+            t.Translate(scrollSpeed * Time.deltaTime);
+        }
+
+        float leftX = mainCamera.transform.position.x - xDistance * 1.5f;
+        float rightX = mainCamera.transform.position.x + xDistance * 1.5f;
+
+        if (objects[0].position.x < leftX) {
+            // move to right side
+            objects[0].position = objects[objects.Count - 1].position + new Vector3(xDistance, 0f, 0f);
+            Transform moveTarget = objects[0];
+            objects.RemoveAt(0);
+            objects.Add(moveTarget);
+        }
+
+        if (objects[objects.Count - 1].position.x > rightX) {
+            // move to left side
+            objects[objects.Count - 1].position = objects[0].position - new Vector3(xDistance, 0f, 0f);
+            Transform moveTarget = objects[objects.Count - 1];
+            objects.RemoveAt(objects.Count - 1);
+            objects.Insert(0, moveTarget);
+        }
+    }
+}
